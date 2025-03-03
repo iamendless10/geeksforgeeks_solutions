5516c4f2b0d300641939a9fc6018e343648a9d48
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-03-03 10:01:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of paths/number-of-paths.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  private:
+    int cc(int i, int j, int m, int n, vector<vector<int>> &dp){
+        if(i == m - 1 && j == n - 1) return 1;
+        
+        if(i > m || j > n) return 0;
+        
+        if(dp[i][j] != - 1) return dp[i][j];
+        
+        return dp[i][j] = cc(i + 1, j, m, n,dp) + cc(i , j + 1, m, n,dp);
+    }
+  public:
+    int numberOfPaths(int m, int n) {
+        // Code Here
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1 , -1));
+        return cc(0,0,m,n,dp);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int m, n;
+        cin >> m >> n;
+        cin.ignore();
+        Solution ob;
+        cout << ob.numberOfPaths(m, n) << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
